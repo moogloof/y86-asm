a7dc31cfe3db6ae0c453c008cd7b9a4d0f31a2f5
moogloof <ethansyun@icloud.com>
2024-02-20 10:41:57 -0800

    base toekn class

--- Token.java
+++ Token.java
@@ -1,2 +1,19 @@
 class Token {
+	enum TokenType {
+		LABEL,
+		DIRECTIVE,
+		INSTRUCTION
+	}
+
+	private TokenType type;
+	private String[] words;
+
+	public Token(TokenType type, String[] words) {
+		this.words = words;
+		this.type = type;
+	}
+
+	// Takes in current address and then returns how much to add to that address
+	int compile(int currentAddr) {
+	}
 }
