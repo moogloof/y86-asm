15c0e2b5d30104a680da0b3c26118f8a69667dbe
moogloof <ethansyun@icloud.com>
2024-02-28 15:57:19 -0800

    Added some compilation stuff for the directives and labels

--- Token.java
+++ Token.java
@@ -7,23 +7,42 @@ class Token {
 		INSTRUCTION
 	}
 
+	private static Hashtable<String, Integer> label_table = new Hashtable<>();
+
 	private TokenType type;
 	private String[] words;
+	private StringBuilder compiled;
 
 	public Token(TokenType type, String[] words) {
 		this.words = words;
 		this.type = type;
+		this.compiled = new StringBuilder();
 	}
 
 	// Takes in current address and then returns how much to add to that address
 	int compile(int currentAddr) {
 		switch (type) {
 			case LABEL:
+				label_table.put(words[0], currentAddr);
 				return 0;
 			case DIRECTIVE:
-				break;
+				if (words[0].equals(".pos")) {
+					// TODO: Handle case where this may be negative, to error out
+					return Integer.decode(words[1]) - currentAddr;
+				} else if (words[0].equals(".align")) {
+					int alignValue = Integer.decode(words[1]);
+					int leftoverPart = currentAddr % alignValue;
+					return (alignValue - leftoverPart) % alignValue;
+				} else if (words[0].equals(".long") || words[0].equals(".quad")) {
+					int fillValue = Integer.decode(words[1]);
+					for (int i = 0; i < 8; i++) {
+						compiled.append((char)(fillValue % 0xff));
+						fillValue /= 0x100;
+					}
+					return 8;
+				}
 			case INSTRUCTION:
-				break;
+				return 0;
 		}
 		return 0;
 	}
