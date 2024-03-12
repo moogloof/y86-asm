0ffd61bdb486765ff265059cf7565a47efc0ed11
moogloof <ethansyun@icloud.com>
2024-03-11 19:38:23 -0700

    Fixed tabbing

--- WordSplitter.java
+++ WordSplitter.java
@@ -3,35 +3,35 @@ import java.io.FileNotFoundException;  // Import this class to handle errors
 import java.util.ArrayList;
 import java.util.Scanner; // Import the Scanner class to read text files
 public class WordSplitter {
-    private String filePath;
+	private String filePath;
 
-    public WordSplitter(String fileName) {
-        filePath = fileName;
-    }
+	public WordSplitter(String fileName) {
+		filePath = fileName;
+	}
 
-    public ArrayList<String> splitWords() throws FileNotFoundException {
-        ArrayList<String> wordsList = new ArrayList<>();
-        File myObj = new File(filePath); //"./yasTest.txt" #comments
+	public ArrayList<String> splitWords() throws FileNotFoundException {
+		ArrayList<String> wordsList = new ArrayList<>();
+		File myObj = new File(filePath); //"./yasTest.txt" #comments
 	if (!myObj.exists()) {
 		throw new FileNotFoundException("Assembly file at " + filePath + " not found.");
 	}
-        Scanner myReader = new Scanner(myObj);
+		Scanner myReader = new Scanner(myObj);
 
-        while (myReader.hasNextLine()) {
-        String data = myReader.nextLine();
-        if(data.contains("#")) {data = data.substring(0,data.indexOf("#"));}
-        String[] arr = data.split("\s");
-        for ( String ss : arr) {
-            String wee = ss.replaceAll("\\s","");
-            if(!wee.equals("")) {
-                wordsList.add(wee);
-            }
-        }
-        }
-        myReader.close();
+		while (myReader.hasNextLine()) {
+		String data = myReader.nextLine();
+		if(data.contains("#")) {data = data.substring(0,data.indexOf("#"));}
+		String[] arr = data.split("\s");
+		for ( String ss : arr) {
+			String wee = ss.replaceAll("\\s","");
+			if(!wee.equals("")) {
+			wordsList.add(wee);
+			}
+		}
+		}
+		myReader.close();
 	return wordsList;
-    }
+	}
 
-    public static void main(String[] args) {
-    }
+	public static void main(String[] args) {
+	}
 }
--- WordTokenizer.java
+++ WordTokenizer.java
@@ -4,57 +4,57 @@ import java.io.*;
 
 public class WordTokenizer {
 
-    private ArrayList<Token> listofTokens = new ArrayList<>();
+	private ArrayList<Token> listofTokens = new ArrayList<>();
 
-    public WordTokenizer() {
+	public WordTokenizer() {
 
-    }
+	}
 
-    public void createTokens(ArrayList<String> wordsList) {
-        int n = wordsList.size();
-        int i = 0;
-        while (i < n) {
-            String currentWord = wordsList.get(i);
-            if(currentWord.indexOf(".")==0) {
-                String[] words = {currentWord,wordsList.get(i+1)};
-                Token directive = new Token(Token.TokenType.DIRECTIVE, words);
-                listofTokens.add(directive);
-                i = i+2;
-            } else if (currentWord.contains(":")) {
-                String[] words = {currentWord};
-                Token label = new Token(Token.TokenType.LABEL, words);
-                listofTokens.add(label);
-                i++;
-            } else {
-                if (currentWord.equals("halt") || currentWord.equals("nop") || currentWord.equals("ret")) {
-                    String[] words = {currentWord};
-                    Token instruction = new Token(Token.TokenType.INSTRUCTION, words);
-                    listofTokens.add(instruction);
-                    i++;
-                } else if (currentWord.indexOf("j") == 0 || currentWord.indexOf("call") == 0 ||
-                        currentWord.indexOf("push") == 0 || currentWord.indexOf("pop") == 0) {
-                    String[] words = {currentWord,wordsList.get(i+1)};
-                    Token instruction = new Token(Token.TokenType.INSTRUCTION, words);
-                    listofTokens.add(instruction);
-                    i = i+2;
-                } else {
-                    String[] words = {currentWord,wordsList.get(i+1), wordsList.get(i+2)};
-                    Token instruction = new Token(Token.TokenType.INSTRUCTION, words);
-                    listofTokens.add(instruction);
-                    i = i+3;
-                }
+	public void createTokens(ArrayList<String> wordsList) {
+		int n = wordsList.size();
+		int i = 0;
+		while (i < n) {
+			String currentWord = wordsList.get(i);
+			if(currentWord.indexOf(".")==0) {
+				String[] words = {currentWord,wordsList.get(i+1)};
+				Token directive = new Token(Token.TokenType.DIRECTIVE, words);
+				listofTokens.add(directive);
+				i = i+2;
+			} else if (currentWord.contains(":")) {
+				String[] words = {currentWord};
+				Token label = new Token(Token.TokenType.LABEL, words);
+				listofTokens.add(label);
+				i++;
+			} else {
+				if (currentWord.equals("halt") || currentWord.equals("nop") || currentWord.equals("ret")) {
+					String[] words = {currentWord};
+					Token instruction = new Token(Token.TokenType.INSTRUCTION, words);
+					listofTokens.add(instruction);
+					i++;
+				} else if (currentWord.indexOf("j") == 0 || currentWord.indexOf("call") == 0 ||
+						currentWord.indexOf("push") == 0 || currentWord.indexOf("pop") == 0) {
+					String[] words = {currentWord,wordsList.get(i+1)};
+					Token instruction = new Token(Token.TokenType.INSTRUCTION, words);
+					listofTokens.add(instruction);
+					i = i+2;
+				} else {
+					String[] words = {currentWord,wordsList.get(i+1), wordsList.get(i+2)};
+					Token instruction = new Token(Token.TokenType.INSTRUCTION, words);
+					listofTokens.add(instruction);
+					i = i+3;
+				}
 
-            }
+			}
 
-        }
-    }
+		}
+	}
 
-    public ArrayList<Token> getListofTokens() {
-        return listofTokens;
-    }
+	public ArrayList<Token> getListofTokens() {
+		return listofTokens;
+	}
 
-    public static void main(String[] args) {
-        WordSplitter blob = new WordSplitter("./yasTest.txt");
+	public static void main(String[] args) {
+		WordSplitter blob = new WordSplitter("./yasTest.txt");
 	try {
 		File outfile = new File("out.bin");
 		FileOutputStream outfile_stream = new FileOutputStream(outfile);
@@ -67,33 +67,33 @@ public class WordTokenizer {
 		tokenizer.createTokens(x);
 		ArrayList<Token> listy = tokenizer.getListofTokens();
 		for(Token ss : listy) {
-		    try {
-		    	pos += ss.compile(pos);
-		    } catch (Exception e) {
-			    System.out.println(e.getMessage());
-			    return;
-		    }
+			try {
+				pos += ss.compile(pos);
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+				return;
+			}
 		}
 		for(Token ss : listy) {
-		    System.out.println(ss);
-		    System.out.println();
+			System.out.println(ss);
+			System.out.println();
 		}
 		for(Token ss : listy) {
-		    ByteArrayOutputStream linked_stuff;
-		    try {
-		    	linked_stuff = ss.link();
-		    } catch (Exception e) {
-			    System.out.println(e.getMessage());
-			    return;
-		    }
-		    try {
+			ByteArrayOutputStream linked_stuff;
+			try {
+				linked_stuff = ss.link();
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+				return;
+			}
+			try {
 			linked_stuff.writeTo(outfile_stream);
-		    } catch (IOException e) {
-		    }
+			} catch (IOException e) {
+			}
 		}
 	} catch (FileNotFoundException e) {
 	}
-    }
+	}
 
 
 }
